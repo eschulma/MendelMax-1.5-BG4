f9fc3a609016002e9994e8da1e8860346f0d23dc
Erica S. Kane <ekane@sentrylink.com>
2020-05-29 17:10:29 -0400

    Lower default e-jerk, as used by linear advance, to match the (unused) classic value.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -793,7 +793,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    2.5  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
